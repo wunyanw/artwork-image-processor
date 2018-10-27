9f4efb761cb71c714d8fce0f1af7c85e92c25050
wunyanw <43711449+wunyanw@users.noreply.github.com>
2018-10-27 08:38:24 -0500

    Update settings.py

--- artwork_image_processor_site/settings.py
+++ artwork_image_processor_site/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
