a3ba087e9edbfe4f7c3616e0cb8d81f8f84d570b
wunyanw <43711449+wunyanw@users.noreply.github.com>
2018-10-28 17:42:29 -0500

    Update settings.py

--- artwork_image_processor_site/settings.py
+++ artwork_image_processor_site/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middlware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'artwork_image_processor_site.urls'
