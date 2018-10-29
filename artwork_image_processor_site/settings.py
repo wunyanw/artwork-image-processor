b92d1e16621306ae8fdad811b391b03038cb1a7d
wunyanw <43711449+wunyanw@users.noreply.github.com>
2018-10-28 19:31:44 -0500

    Update settings.py

--- artwork_image_processor_site/settings.py
+++ artwork_image_processor_site/settings.py
@@ -11,12 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-#try:
-    # Configure Django App for Heroku.
-    ## Activate Django-Heroku.
 import django_heroku
-#except ImportError:
-#    found = False
+# Configure Django App for Heroku.
+## Activate Django-Heroku.
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
