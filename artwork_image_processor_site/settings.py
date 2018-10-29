93cda734e0933ce5b8154b347054a9d70f682dfc
wunyanw <43711449+wunyanw@users.noreply.github.com>
2018-10-28 19:05:10 -0500

    Update settings.py

--- artwork_image_processor_site/settings.py
+++ artwork_image_processor_site/settings.py
@@ -20,9 +20,9 @@ except ImportError:
     found = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
